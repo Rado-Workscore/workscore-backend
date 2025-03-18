abb9dd4e169e88bf2212fcf46b7bfcd07a2d2c27
Barss Rado <barssrado@mac.home>
2025-03-18 16:37:10 -0400

    Updated DATABASE_URL with Render PostgreSQL URL

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from databases import Database
 import sqlalchemy
 
-DATABASE_URL = "postgresql://barssrado@localhost/workscore_db"
+DATABASE_URL = "postgresql://workscore_db_user:himcx6zc5rcHirVIKfmSSTh0eI5CgBmM@dpg-cvctgdtrie7s739jhvn0-a.oregon-postgres.render.com/workscore_db"
 
 database = Database(DATABASE_URL)
 metadata = sqlalchemy.MetaData()
